c8b7d8eeba86b3d7537be0dbec0c2b4230b99c0e
sebastian2852 <sebastian2852@proton.me>
2025-06-23 01:26:41 +0100

    Added comments to package

--- Lib/Source/index.ts
+++ Lib/Source/index.ts
@@ -1,5 +1,11 @@
 import { RunService } from "@rbxts/services";
 
+/**
+ * A QueR acts like a queue for your tasks. It stores
+ * all the tasks you give it and then act upon them every
+ * so often. This can help you batch operations like
+ * invoking remote functions/events.
+ */
 export default class QueR<T extends defined> {
 	private m_FlushInterval;
 	private m_Connection;
@@ -9,12 +15,22 @@ export default class QueR<T extends defined> {
 	private m_Tasks = new Array<T>();
 	private m_Destroyed = false;
 
+	/**
+	 * Destroy the QueR
+	 * This forces a final flush as well as disconnecting the
+	 * internal heartbeat connection. This also prevents any
+	 * future tasks from being added
+	 */
 	public Destroy() {
 		this.m_Connection.Disconnect();
 		this.Flush();
 		this.m_Destroyed = true;
 	}
 
+	/**
+	 * Adds an item to be handled on the next flush.
+	 * @param {T} item - The item to add
+	 */
 	public Add(item: T) {
 		this.m_Tasks.push(item);
 	}
@@ -24,6 +40,10 @@ export default class QueR<T extends defined> {
 		this.m_Tasks = new Array<T>();
 	}
 
+  /**
+   * @param {number} flushInterval - How often the QueR should flush in seconds
+   * @param {(data: Array<T>) => void} handler - Handler function called on each flush
+   */
 	constructor(flushInterval: number, handler: (data: Array<T>) => void) {
 		this.m_FlushInterval = flushInterval;
 		this.m_Handler = handler;
